f555e53668a0f6024aecb0bd33efb7cc8fa6a479
Andrew <andrewsouthard111@gmail.com>
2017-03-08 18:46:46 -0500

    Add first letter functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def param_page
     @name = params["name"].upcase
+    @message = ""
+    if @name[0] == 'A'
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    end
     render 'param_guess.html.erb'
   end
 end
