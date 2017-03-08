af337a3bffb87309dfeacb2f389d51d03b50cdf8
Andrew <andrewsouthard111@gmail.com>
2017-03-08 18:56:04 -0500

    Add guess_num method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,18 @@ class PagesController < ApplicationController
     if @name[0] == 'A'
       @message = "Hey, your name starts with the first letter of the alphabet!"
     end
+    guess_num(63)
     render 'param_guess.html.erb'
   end
+
+  def guess_num(num)
+    @number = params["number"].to_i
+    if num > @number
+      @game_line = "Your guess is too low."
+    elsif num < @number
+      @game_line = "Your guess is too high."
+    else
+      @game_line = "You got it right! The number was #{num}"
+    end
+  end      
 end
