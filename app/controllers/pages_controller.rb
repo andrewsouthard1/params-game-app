d54e908e4cad3c8e679149f7be58944193a0146a
Andrew <andrewsouthard111@gmail.com>
2017-03-08 19:05:48 -0500

    Add button_click view, route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,27 @@ class PagesController < ApplicationController
   def param_page
     @name = params["name"].upcase
     @message = ""
-    if @name[0] == 'A'
-      @message = "Hey, your name starts with the first letter of the alphabet!"
-    end
+    @message = "Hey, your name starts with the first letter of the alphabet!" if @name[0] == 'A'
     guess_num(63)
     render 'param_guess.html.erb'
   end
 
   def guess_num(num)
     @number = params["number"].to_i
-    if num > @number
-      @game_line = "Your guess is too low."
-    elsif num < @number
-      @game_line = "Your guess is too high."
-    else
-      @game_line = "You got it right! The number was #{num}"
-    end
-  end      
+    @game_line =
+      if num > @number
+        "Your guess is too low."
+      elsif num < @number
+        "Your guess is too high."
+      else
+        "You got it right! The number was #{num}"
+      end
+  end
+
+  def button_click
+    render 'button_click.html.erb'
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/param_page' => 'pages#param_page'
+  get '/button_click' => 'pages#button_click'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
